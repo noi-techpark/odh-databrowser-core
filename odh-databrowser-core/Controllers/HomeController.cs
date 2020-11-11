8cb394d233e6791897685372c55cf9232e5d9536
Tobias Burger <tobias_burger@hotmail.com>
2020-11-11 16:42:12 +0100

    Getting the basic skeleton to work

--- odh-databrowser-core/Controllers/HomeController.cs
+++ odh-databrowser-core/Controllers/HomeController.cs
@@ -1,37 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using odh_databrowser_core.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
-namespace odh_databrowser_core.Controllers
+namespace SuedtirolManagerPG.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly NpgsqlConnection _connection;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(/*NpgsqlConnection connection*/)
         {
-            _logger = logger;
+            //_connection = connection;
+            _connection = default;
         }
 
-        public IActionResult Index()
+        //[HasPermission("IndexViewer")]
+        public ActionResult Index()
         {
+            ViewBag.Title = "Home Page";
+
+            //Display Rights:
+
+            //var user = User.Identity.Name;
+            //var user2 = System.Web.HttpContext.Current.Request.LogonUserIdentity.Name;
+
+            //var cookies = this.ControllerContext.HttpContext.Request.Cookies;
+
+
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult LicenseStatus()
         {
+            ViewBag.Title = "CC0 License State";
+
+            //Query PG 
+            ViewBag.AccoLicenses = GetCC0Licenses("accommodations");
+            ViewBag.GastroLicenses = GetCC0Licenses("gastronomies");
+            ViewBag.EventLicenses = GetCC0Licenses("events");
+            ViewBag.PoiLicenses = GetCC0Licenses("pois");
+            ViewBag.ActivityLicenses = GetCC0Licenses("activities");
+            ViewBag.RegionLicenses = GetCC0Licenses("regions");
+            ViewBag.TVLicenses = GetCC0Licenses("tvs");
+            ViewBag.SkiAreaLicenses = GetCC0Licenses("skiareas");
+            ViewBag.ODHActivityPoiLicenses = GetCC0Licenses("smgpois");
+
+            ViewBag.AccoTotaldata = GetTotalCount("accommodations");
+            ViewBag.GastroTotaldata = GetTotalCount("gastronomies");
+            ViewBag.EventTotaldata = GetTotalCount("events");
+            ViewBag.PoiTotaldata = GetTotalCount("pois");
+            ViewBag.ActivityTotaldata = GetTotalCount("activities");
+            ViewBag.RegionTotaldata = GetTotalCount("regions");
+            ViewBag.TVTotaldata = GetTotalCount("tvs");
+            ViewBag.SkiAreaTotaldata = GetTotalCount("skiareas");
+            ViewBag.ODHActivityPoiTotaldata = GetTotalCount("smgpois");
+
+            ViewBag.AccoTotaldataopen = GetTotalCount("accommodationsopen");
+            ViewBag.GastroTotaldataopen = GetTotalCount("gastronomiesopen");
+            ViewBag.EventTotaldataopen = GetTotalCount("eventsopen");
+            ViewBag.PoiTotaldataopen = GetTotalCount("poisopen");
+            ViewBag.ActivityTotaldataopen = GetTotalCount("activitiesopen");
+            ViewBag.RegionTotaldataopen = GetTotalCount("regionsopen");
+            ViewBag.TVTotaldataopen = GetTotalCount("tvsopen");
+            ViewBag.SkiAreaTotaldataopen = GetTotalCount("skiareasopen");
+            ViewBag.ODHActivityPoiTotaldataopen = GetTotalCount("smgpoisopen");
+
+            ViewBag.AccoImagesopenCount = GetTotalImagesCount("accommodations");
+            ViewBag.GastroImagesopenCount = GetTotalImagesCount("gastronomies");
+            ViewBag.EventImagesopenCount = GetTotalImagesCount("events");
+            ViewBag.PoiImagesopenCount = GetTotalImagesCount("pois");
+            ViewBag.ActivityImagesopenCount = GetTotalImagesCount("activities");
+            ViewBag.RegionImagesopenCount = GetTotalImagesCount("regions");
+            ViewBag.TVImagesopenCount = GetTotalImagesCount("tvs");
+            ViewBag.SkiAreaImagesopenCount = GetTotalImagesCount("skiareas");
+            ViewBag.ODHActivityPoiImagesopenCount = GetTotalImagesCount("smgpois");
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private long GetCC0Licenses(string tablename)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            long result = 0;
+
+            using (var conn = _connection)
+            {
+                conn.Open();
+               
+                // TODO: implement
+                //result = Common.PGLicenseHelper.GetAllLicenses(conn, tablename);
+                
+                conn.Close();
+
+            }
+
+            return result;
+        }
+
+        private long GetTotalCount(string tablename)
+        {
+            long result = 0;
+
+            using (var conn = _connection)
+            {
+                conn.Open();
+
+                // TODO: implement
+                //result = Common.PostgresSQLHelper.CountDataFromTable(conn, tablename, "");
+
+                conn.Close();
+            }
+
+            return result;
+        }
+
+        private long GetTotalImagesCount(string tablename)
+        {
+            long result = 0;
+
+            using (var conn = _connection)
+            {
+                conn.Open();
+
+                // TODO: implement
+                //result = Common.PGLicenseHelper.GetAllImagesWithLicenseCount(conn, tablename);
+
+                conn.Close();
+            }
+
+            return result;
         }
     }
 }

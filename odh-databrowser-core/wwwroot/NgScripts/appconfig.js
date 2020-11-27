2cf6024c8fae6a521235ae8041262a9f5b065521
Rudi Thoeni <rudi.thoeni@hotmail.com>
2020-11-27 16:09:47 +0100

    updated config

--- odh-databrowser-core/wwwroot/NgScripts/appconfig.js
+++ odh-databrowser-core/wwwroot/NgScripts/appconfig.js
@@ -2,7 +2,8 @@
 var appconfig = angular.module('appconfig', ['appfactory', 'ngRoute'])
 
 appconfig.value('appconfig', {
-      basePath: 'https://api.tourism.testingmachine.eu/' // Set your base path here
+      basePath: 'https://api.tourism.testingmachine.eu' // Set your base path here
+    //basePath: 'https://localhost:5001' // Set your base path here
   });
 
 appconfig.config(function ($httpProvider) {

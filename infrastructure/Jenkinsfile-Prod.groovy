c3a0a9f2e6f08198a4ad78ccdae66c1aca469a99
RudiThoeni <30471361+RudiThoeni@users.noreply.github.com>
2021-01-19 17:19:51 +0100

    Update Jenkinsfile-Prod.groovy

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -6,6 +6,8 @@ pipeline {
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/odh-databrowser-core'
         DOCKER_TAG = "prod-$BUILD_NUMBER"
 	SERVER_PORT = "1022"        
+	OAUTH_AUTORITY = "https://auth.opendatahub.bz.it/auth/realms/noi/"
+	OAUTH_CLIENTID = credentials('odh-tourism-frontend-oauth-clientid')
     }
 
     stages {
@@ -18,6 +20,8 @@ pipeline {
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
                     echo 'SERVER_PORT=${SERVER_PORT}' >> .env                    
+		    echo 'OAUTH_AUTORITY=${OAUTH_AUTORITY}' >> .env   
+		    echo 'OAUTH_CLIENTID=${OAUTH_CLIENTID}' >> .env   
                 """
             }
         }

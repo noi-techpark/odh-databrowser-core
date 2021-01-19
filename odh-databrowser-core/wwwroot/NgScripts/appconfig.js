f4897ef761aa3e7d17acc1dafd68cc0c18820c5c
RudiThoeni <30471361+RudiThoeni@users.noreply.github.com>
2021-01-19 17:54:51 +0100

    setting prod api

--- odh-databrowser-core/wwwroot/NgScripts/appconfig.js
+++ odh-databrowser-core/wwwroot/NgScripts/appconfig.js
@@ -2,10 +2,10 @@
 var appconfig = angular.module('appconfig', ['appfactory', 'ngRoute'])
 
 appconfig.value('appconfig', {
-    basePath: 'https://api.tourism.testingmachine.eu' // odh-api-core on testingmachine
+    //basePath: 'https://api.tourism.testingmachine.eu' // odh-api-core on testingmachine
     //basePath: 'https://localhost:5001' // Local odh-api-core URL
     //basePath: 'https://localhost:44322' // Local odh-api-mongo-core URL
-     //basePath: 'https://tourism.api.opendatahub.bz.it' // odh-api-core on production
+    basePath: 'https://tourism.api.opendatahub.bz.it' // odh-api-core on production
   });
 
 appconfig.config(function ($httpProvider) {

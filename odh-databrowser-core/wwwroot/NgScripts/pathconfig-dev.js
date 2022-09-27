885d8c850b5abf0652ab1a7d18a754712fce2b85
RudiThoeni <r.thoeni@noi.bz.it>
2022-09-27 13:18:43 +0200

    changing oauth url

--- odh-databrowser-core/wwwroot/NgScripts/pathconfig-dev.js
+++ odh-databrowser-core/wwwroot/NgScripts/pathconfig-dev.js
@@ -2,7 +2,7 @@
 var pathconfig = angular.module('pathconfig', [])
     //.constant('apipath', 'https://localhost:5001')
     .constant('apipath', 'https://api.tourism.testingmachine.eu')
-    .constant('authserverpath', 'https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/auth?client_id=odh-frontend-core&response_type=token&redirect_uri=');
+    .constant('authserverpath', 'https://authupgrade.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/auth?client_id=odh-frontend-core&response_type=token&redirect_uri=');
 
 
 

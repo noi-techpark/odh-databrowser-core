f00fadbb1908285758ff793ed27b0a751fc98751
RudiThoeni <r.thoeni@noi.bz.it>
2023-10-10 09:13:53 +0200

    adjusting api url

--- odh-databrowser-core/wwwroot/NgScripts/pathconfig-dev.js
+++ odh-databrowser-core/wwwroot/NgScripts/pathconfig-dev.js
@@ -4,8 +4,8 @@
 
 // Path Config Constants
 var pathconfig = angular.module('pathconfig', [])
-    .constant('apipath', 'https://localhost:5001')
-    //.constant('apipath', 'https://api.tourism.testingmachine.eu')
+    //.constant('apipath', 'https://localhost:5001')
+    .constant('apipath', 'https://api.tourism.testingmachine.eu')
     .constant('authserverpath', 'https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/auth?client_id=odh-frontend-core&response_type=token&redirect_uri=');
 
 
